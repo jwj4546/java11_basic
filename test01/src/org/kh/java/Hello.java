852743ecac87b699b2ee630fbfa7fb145c21f85c
jwj4546 <jwj4546@naver.com>
2024-02-21 14:10:15 +0900

    java 11 installation & variable & operation

---
+++ test01/src/org/kh/java/Hello.java
@@ -0,0 +1,16 @@
+package org.kh.java;
+
+public class Hello {
+
+	public static void main(String[] args) {		//실행을 위한 기본 main 메소드 = Ctrl + / = 한줄 각주
+		System.out.println("Hello~! \nJAVA~!");
+		/*
+		 자기 이름 소개하는
+		 출력 명령
+		 결과 : Myname is WooJinDevil = Ctrl + Shift + / = 여러 줄 각주
+		 */
+		//sysout => Ctrl + Spacebar = 단축 명령 자동 완성(Code Hinting)
+		System.out.println("My name is WooJinDevil");
+	}
+
+}
